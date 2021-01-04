acf98af5016c091b70ffafc68b4c08fdfa4b6e62
augustomtt <65096407+augustomtt@users.noreply.github.com>
2021-01-04 11:13:07 -0300

    Format: Indentation

--- detectivetracker/lua/weapons/weapon_ttt_damagetracker.lua
+++ detectivetracker/lua/weapons/weapon_ttt_damagetracker.lua
@@ -1,15 +1,19 @@
 --Basics
-SWEP.PrintName = "Gunpowder Finder"
-SWEP.Instructions = "Left mouse to check if a player has been shot by a player this round"
+SWEP.PrintName = "Detective Tracker"
+SWEP.Instructions = "Left click: Add a tracker to a player - Right click: Check if the tracked player has been shot by a player this round"
+
 --Creator
 SWEP.Author = "Angus"
 SWEP.Contact = "https://www.steamcommunity.com/profiles/76561198065084199"
+
 --
 SWEP.Spawnable = true
 SWEP.AdminOnly = false
+
 -- Always derive from weapon_tttbase.
 SWEP.Base = "weapon_tttbase"
 SWEP.Kind = WEAPON_EQUIP1
+
 --Primary click
 SWEP.Primary.ClipSize = 1
 SWEP.Primary.DefaultClip = 1
@@ -37,75 +41,73 @@ SWEP.AutoSpawnable = false
 SWEP.CanBuy = {ROLE_DETECTIVE}
 
 SWEP.LimitedStock = true
-SWEP.AllowDrop = true
+SWEP.AllowDrop = false --haven't test this yet
 
 SWEP.EquipMenuData = {
-	type = "Weapon",
-	desc = "This is a test lalala"
+  type = "Equipment",
+  desc = "Left click: Add a tracker to a player - Right click: Check if the tracked player has been shot by a player this round"
 }
 
-function PlayerWithinBounds(ply,otherPly, dist)
-	return ply:GetPos():DistToSqr(otherPly:GetPos()) < (dist * dist)
+function PlayerWithinBounds(ply, otherPly, dist)
+  return ply:GetPos():DistToSqr(otherPly:GetPos()) < (dist * dist)
 end
-
-
 
 function SWEP:Initialize()
-	if SERVER then
-		trackedply = nil
-		trackerply = nil
-		attackers = {}
-		table.Empty(attackers)
-	end
+  if SERVER then
+    trackedply = nil
+    trackerply = nil
+    attackers = {}
+    table.Empty(attackers)
+    self:SetHoldType("pistol")
+  end
 end
 
-
 function SWEP:PrimaryAttack()
-	if (self:CanPrimaryAttack()) then
-		self:SetNextPrimaryFire(CurTime() + 1)
-		self:TakePrimaryAmmo(1)
-		self:EmitSound(self.ShootSound)
+  if (self:CanPrimaryAttack()) then
+    self:SetNextPrimaryFire(CurTime() + 1)
+    self:TakePrimaryAmmo(1)
+    self:EmitSound(self.ShootSound)
 
-		if SERVER then
-			--was the detective able to hit the tracker?
-			hook.Add("EntityTakeDamage", "hitDetectiveCheck", function(target, dmginfo)
-				if (IsValid(target) and target:IsPlayer() and dmginfo:IsBulletDamage() and dmginfo:GetAttacker():GetActiveWeapon() == self) then
-					dmginfo:GetAttacker():PrintMessage(HUD_PRINTTALK, "Tracker added to: " .. target:GetName())
-					trackedply = target
-					trackerply = dmginfo:GetAttacker()
-				end
-			end)
-			
-			self:ShootBullet(0, 1, 0.02)
-			table.Empty(attackers)
-			timer.Simple(1, function() 
-				if (trackedply ~= nil) then
-					hook.Add("EntityTakeDamage","trackuser", function(trackTarget,trackinfo)
-					if (trackTarget == trackedply and IsPlayer(trackinfo:GetAttacker()) and trackinfo:GetAttacker() ~= target and not table.HasValue(attackers, trackinfo:GetAttacker())) then
-						table.insert(attackers, trackinfo:GetAttacker())	
-					end
-					end)
-				end 
-			end)
+    if SERVER then
+      --was the detective able to hit the tracker?
+      hook.Add("EntityTakeDamage", "hitDetectiveCheck", function(target, dmginfo)
+        if (IsValid(target) and target:IsPlayer() and dmginfo:IsBulletDamage() and dmginfo:GetAttacker():GetActiveWeapon() == self) then
+          dmginfo:GetAttacker():PrintMessage(HUD_PRINTTALK, "Tracker added to: " .. target:GetName())
+          trackedply = target
+          trackerply = dmginfo:GetAttacker()
+        end
+      end)
 
-			timer.Simple(1, function()
-				hook.Remove("EntityTakeDamage", "hitDetectiveCheck")
-			end)
+      self:ShootBullet(0, 1, 0.02)
+      table.Empty(attackers)
 
-		end
-	end
+      timer.Simple(1, function()
+        if (trackedply ~= nil) then
+          hook.Add("EntityTakeDamage", "trackuser", function(trackTarget, trackinfo)
+            if (trackTarget == trackedply and IsPlayer(trackinfo:GetAttacker()) and trackinfo:GetAttacker() ~= target and not table.HasValue(attackers, trackinfo:GetAttacker())) then
+              table.insert(attackers, trackinfo:GetAttacker())
+            end
+          end)
+        end
+      end)
+
+      timer.Simple(1, function()
+        hook.Remove("EntityTakeDamage", "hitDetectiveCheck")
+      end)
+    end
+  end
 end
 
 function SWEP:SecondaryAttack()
-	if SERVER and (trackedply ~= nil and trackerply ~= nil) and (PlayerWithinBounds(trackerply,trackedply,200)) then
-		if (not trackedply:Alive()) then
-			trackerply:PrintMessage(HUD_PRINTTALK, "The player is dead and the tracker was destroyed")
-		elseif (table.IsEmpty(attackers)) then
-			trackerply:PrintMessage(HUD_PRINTTALK, "This player didn't receive damage from other players")
-		else
-			for i,k in pairs (attackers) do
-				trackerply:PrintMessage(HUD_PRINTTALK, "The player received damage from " .. k:GetName())
-			end
-		end
-	end
+  if SERVER and (trackedply ~= nil and trackerply ~= nil) and (PlayerWithinBounds(trackerply, trackedply, 200)) then
+    if (not trackedply:Alive()) then
+      trackerply:PrintMessage(HUD_PRINTTALK, "The player is dead and the tracker was destroyed")
+    elseif (table.IsEmpty(attackers)) then
+      trackerply:PrintMessage(HUD_PRINTTALK, "This player didn't receive damage from other players")
+    else
+      for i, k in pairs(attackers) do
+        trackerply:PrintMessage(HUD_PRINTTALK, "The player received damage from " .. k:GetName())
+      end
+    end
+  end
 end
